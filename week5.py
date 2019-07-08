9be7f2ea73a18a54110f38099ec0469d90dc7e75
Samuel Kratsas <skratsas17@mail.bw.edu>
2019-07-08 12:30:00 -0400

    Create week5.py

---
+++ week5.py
@@ -0,0 +1,20 @@
+#Chapter 11 Try its:
+
+#N[SEM='Population'] -> 'populations'
+#CardN[SEM='1000'] -> '1,000,000' 
+#Conj[SEM='AND'] -> 'and'
+#TV[SEM=''] -> 'have'
+#P[SEM='>'] -> 'above'
+
+# Yes, works very similiar to logic in discrete functions
+
+#Picture: https://docs.google.com/document/d/1PlnQGlxkfaLKDYuh_a9AgOhGHUoRXsrxwPupmYHQ6xk/edit
+
+#v2 = """
+#bruce => b
+#elspeth => e
+#julia => j
+#matthew => m
+#person => {b, e, j, m}
+#admire => {(b, j), (b, e), (b, m),(b,b),(m,b),(m,m),(m,e),(m,j),(e,e),(e,b),(e,j),(e,m),(j,j),(j,e),(j,b),(j,m)}
+#"""
